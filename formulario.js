f4a34d2f1c47839ab7722f502c624092e393ee8f
esteban <91167097+Estebanjgg@users.noreply.github.com>
2021-11-02 20:07:53 -0200

    validando input

--- formulario.js
+++ formulario.js
@@ -8,3 +8,19 @@ const expresiones = {
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
+
+const validarFormulario = () => {
+	
+}
+
+
+inputs.forEach((input) => {
+	input.addEventListener('keyup', () =>{
+		console.log('tecla levantada');
+	});
+});
+
+formulario.addEventListener('sbmit', (e) => {
+	e.preventDefault();
+});
+
